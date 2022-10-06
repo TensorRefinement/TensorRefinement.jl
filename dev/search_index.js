76c4aa6684383662e147b1f1ff7155a36ed80b4b
Documenter.jl <documenter@juliadocs.github.io>
2022-10-06 19:55:47 +0000

    build based on f07d06f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TensorRefinement","category":"page"},{"location":"#TensorRefinement","page":"Home","title":"TensorRefinement","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TensorRefinement]","category":"page"}]
+}
